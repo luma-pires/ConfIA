e0c83b81bf95ea10eb1f854a46a4c95d5a856f8a
Luma Sifuentes <lumacicilia0102@gmail.com>
2025-01-03 09:53:54 -0300

    switched open ai llm to groq and added pinecone api key

--- chatbot.py
+++ chatbot.py
@@ -1,23 +1,24 @@
 import os
 from dotenv import load_dotenv
-from langchain_openai import ChatOpenAI
-from langchain_community.chat_models import ChatOpenAI
 from langchain.schema import (SystemMessage, HumanMessage, AIMessage)
+from langchain_groq import ChatGroq
+from pinecone import Pinecone
 
 
 class ChatBot:
 
     def __init__(self):
-        self.openai_api_key, self.database_password = self.get_env_info()
-        self.chat = ChatOpenAI(model='gpt-3.5-turbo')
+        self.groq_api_key, self.db_api_key = self.get_env_info()
+        self.chat = ChatGroq(temperature=0, model_name="llama-3.1-70b-versatile")
+        self.db = Pinecone(api_key=self.db_api_key)
         self.messages = []
 
     @staticmethod
     def get_env_info():
         load_dotenv('./.env')
-        openai_api_key = os.getenv("OPENAI_API_KEY")
-        database_password = os.getenv("DATABASE_PASSWORD")
-        return openai_api_key, database_password
+        groq_api_key = os.getenv("GROQ_API_KEY")
+        database_password = os.getenv("DB_API_KEY")
+        return groq_api_key, database_password
 
     def write_prompt(self, prompt):
         new_prompt = HumanMessage(prompt)
@@ -25,7 +26,11 @@ class ChatBot:
 
     def chat_answer(self):
         res = self.chat.invoke(self.messages)
+        print(res.content)
 
-        return res
+
+a = ChatBot()
+a.write_prompt("Olá")
+a.chat_answer()
 
 
