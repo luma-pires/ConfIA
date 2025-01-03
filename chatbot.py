3ad7e5c9f84c0f4cbe3d9246e7f60a6461d4be9b
Luma Sifuentes <lumacicilia0102@gmail.com>
2025-01-03 11:37:16 -0300

    Inserted Pinecone and crrently working on learning from user feedback (and if it is positive, save this in db

--- chatbot.py
+++ chatbot.py
@@ -2,7 +2,9 @@ import os
 from dotenv import load_dotenv
 from langchain.schema import (SystemMessage, HumanMessage, AIMessage)
 from langchain_groq import ChatGroq
-from pinecone import Pinecone
+from langchain.embeddings import OpenAIEmbeddings
+import pinecone
+from textblob import TextBlob
 
 
 class ChatBot:
@@ -10,8 +12,12 @@ class ChatBot:
     def __init__(self):
         self.groq_api_key, self.db_api_key = self.get_env_info()
         self.chat = ChatGroq(temperature=0, model_name="llama-3.1-70b-versatile")
-        self.db = Pinecone(api_key=self.db_api_key)
+        self.db = pinecone.init(api_key=self.db_api_key, environment="us-west1-gcp")
+        self.embeddings_model = OpenAIEmbeddings()
         self.messages = []
+        self.index_name = "quickstart"
+        self.index = self.get_index()
+        self.interaction_id = -1
 
     @staticmethod
     def get_env_info():
@@ -20,17 +26,68 @@ class ChatBot:
         database_password = os.getenv("DB_API_KEY")
         return groq_api_key, database_password
 
-    def write_prompt(self, prompt):
-        new_prompt = HumanMessage(prompt)
+    def creating_index(self):
+        self.db.create_index(
+            name=self.index_name,
+            dimension=1024,
+            metric="cosine"
+        )
+
+    def get_index(self):
+        return self.db.Index(self.index_name)
+
+    def search_similar_context(self, query_text):
+        query_embedding = self.embeddings_model.embed(query_text)
+        result = self.index.query(
+            queries=[query_embedding.tolist()],
+            top_k=3,
+            include_values=True
+        )
+        return result
+
+    @staticmethod
+    def get_sentiment(feedback):
+        return TextBlob(feedback).sentiment.polarity
+
+    def write_prompt(self, user_prompt):
+        new_prompt = HumanMessage(user_prompt)
         self.messages.append(new_prompt)
 
     def chat_answer(self):
-        res = self.chat.invoke(self.messages)
-        print(res.content)
+        answer = self.chat.invoke(self.messages)
+        return answer
+
+    def interaction_with_chat(self, user_prompt):
+        self.write_prompt(user_prompt)
+        ai_answer = self.chat_answer()
+        print(ai_answer)
+        return user_prompt, ai_answer
+
+    def store_interaction_in_db(self, user_prompt, ai_answer):
+        user_embedding = self.embeddings_model.embed(user_prompt)
+        ai_answer_embedding = self.embeddings_model.embed(ai_answer)
+        self.interaction_id = self.interaction_id + 1
+        self.index.upsert(
+            [(self.interaction_id, user_embedding.tolist()), (self.interaction_id, ai_answer_embedding.tolist())]
+        )
+
+    def learn_from_user_feedback(self, user_message, feedback):
+        sentiment = self.get_sentiment(feedback)
+        if sentiment > 0:
+            ai_answer = self.chat_answer()
+            self.store_interaction_in_db(user_message, ai_answer)
+
+    def first_touch(self):
+        user_prompt = input("Talk to me!")
+        self.interaction_with_chat(user_prompt)
+
+    def main(self):
+        self.first_touch()
+        while True:
+            user_prompt = input("Talk to me!")
+            self.learn_from_user_feedback(user_prompt)
 
 
 a = ChatBot()
 a.write_prompt("Olá")
 a.chat_answer()
-
-
