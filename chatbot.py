88541e001505c88cea21eabc1f48a83ad2980027
Luma Sifuentes <lumacicilia0102@gmail.com>
2025-01-03 08:40:58 -0300

    FIrst commit in chatbot

---
+++ chatbot.py
@@ -0,0 +1,30 @@
+import os
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import (SystemMessage, HumanMessage, AIMessage)
+
+
+class ChatBot:
+
+    def __init__(self):
+        self.openai_api_key, self.database_password = self.get_env_info()
+        self.chat = ChatOpenAI(model='gpt-3.5-turbo')
+        self.messages = []
+
+    @staticmethod
+    def get_env_info():
+        load_dotenv('./.env')
+        openai_api_key = os.getenv("OPENAI_API_KEY")
+        database_password = os.getenv("DATABASE_PASSWORD")
+        return openai_api_key, database_password
+
+    def write_prompt(self, prompt):
+        new_prompt = HumanMessage(prompt)
+        self.messages.append(new_prompt)
+
+    def chat_answer(self):
+        res = self.chat.invoke(self.messages)
+        return res
+
+
