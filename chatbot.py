9526e85c476ba5b3e7da0016148875cb4cc0b425
Luma Sifuentes <lumacicilia0102@gmail.com>
2025-01-04 20:38:53 -0300

    Now the chat can search context in database!

--- chatbot.py
+++ chatbot.py
@@ -1,93 +1,152 @@
 import os
 from dotenv import load_dotenv
-from langchain.schema import (SystemMessage, HumanMessage, AIMessage)
+from langchain.schema import (HumanMessage)
 from langchain_groq import ChatGroq
-from langchain.embeddings import OpenAIEmbeddings
-import pinecone
-from textblob import TextBlob
+from pinecone import Pinecone, ServerlessSpec
+import re
+from sentence_transformers import SentenceTransformer
+from datetime import datetime
 
 
 class ChatBot:
 
     def __init__(self):
-        self.groq_api_key, self.db_api_key = self.get_env_info()
+        self.groq_api_key, self.openai_api_key, self.db_api_key = self.get_env_info()
         self.chat = ChatGroq(temperature=0, model_name="llama-3.1-70b-versatile")
-        self.db = pinecone.init(api_key=self.db_api_key, environment="us-west1-gcp")
-        self.embeddings_model = OpenAIEmbeddings()
+        self.chat_classifier = self.chat
+        self.db = Pinecone(api_key=self.db_api_key, environment="us-west1-gcp")
+        self.embeddings_model = SentenceTransformer('bert-base-nli-mean-tokens')
         self.messages = []
-        self.index_name = "quickstart"
-        self.index = self.get_index()
-        self.interaction_id = -1
+        self.checking_indexes()
+        self.index_preferences = self.get_index('index-preferences')
+        self.index_corrections = self.get_index('index-corrections')
 
     @staticmethod
     def get_env_info():
         load_dotenv('./.env')
         groq_api_key = os.getenv("GROQ_API_KEY")
+        openai_api_key = os.getenv("OPENAI_API_KEY")
         database_password = os.getenv("DB_API_KEY")
-        return groq_api_key, database_password
+        return groq_api_key, openai_api_key, database_password
 
-    def creating_index(self):
-        self.db.create_index(
-            name=self.index_name,
-            dimension=1024,
-            metric="cosine"
-        )
+    def checking_indexes(self):
+        self.creating_index_if_it_does_not_exists('index-preferences')
+        self.creating_index_if_it_does_not_exists('index-corrections')
 
-    def get_index(self):
-        return self.db.Index(self.index_name)
+    def creating_index_if_it_does_not_exists(self, index_name):
+        existing_indexes = [i['name'] for i in self.db.list_indexes().to_dict()['indexes']]
+        if index_name not in existing_indexes:
+            self.db.create_index(
+                name=index_name,
+                dimension=768,
+                metric="cosine",
+                spec=ServerlessSpec(
+                    cloud="aws",
+                    region="us-east-1"
+                )
+            )
 
-    def search_similar_context(self, query_text):
+    def get_index(self, index_name):
+        return self.db.Index(index_name)
+
+    def search_similar_context(self, query_text, index, k=3):
         query_embedding = self.embeddings_model.embed(query_text)
-        result = self.index.query(
+        result = index.query(
             queries=[query_embedding.tolist()],
-            top_k=3,
+            top_k=k,
             include_values=True
         )
         return result
 
-    @staticmethod
-    def get_sentiment(feedback):
-        return TextBlob(feedback).sentiment.polarity
+    def classifier(self, user_message):
+
+        preference_labels = {'Envolve preferências sobre como a resposta deve ser dada': True,
+                             'Não envolve preferências sobre como a resposta deve ser dada': False}
+
+        corrections_labels = {'É uma correção do usuário': True, 'Não é uma correção do usuário': False}
+
+        detected_classes = self.chat_classifier.invoke(f"Classifique a seguinte mensagem entre as classes\
+                                                       [{" ".join(preference_labels.keys())}] e depois entre as classes\
+                                                       [{" ".join(corrections_labels.keys())}]: \n\
+                                                       Mensagem = {user_message} \n\
+                                                       Retorne apenas o nome das categorias mais prováveis por grupo.\
+                                                       Exemplo de resposta: [Categoria Grupo 1] [Categoria Grupo 2]")
+
+        answer = detected_classes.content
+        classes = re.findall(r"\[(.*?)]", answer)
+
+        preference_class = preference_labels.get(classes[0], None)
+        correction_class = corrections_labels.get(classes[1], None)
+
+        return preference_class, correction_class
+
+    def interaction(self, user_prompt):
+        self.write_prompt(user_prompt)
+        ai_answer = self.informed_chat_answer(user_prompt)
+        print(ai_answer)
 
     def write_prompt(self, user_prompt):
-        new_prompt = HumanMessage(user_prompt)
-        self.messages.append(new_prompt)
+        is_preference, is_correction = self.classifier(HumanMessage(user_prompt))
+        if is_preference:
+            print('Is preference')
+            self.store_interaction_in_db(user_prompt, self.index_preferences, 'preference')
+        if is_correction:
+            print('Is correction')
+            self.store_interaction_in_db(user_prompt, self.index_corrections, 'correction')
 
-    def chat_answer(self):
-        answer = self.chat.invoke(self.messages)
-        return answer
-
-    def interaction_with_chat(self, user_prompt):
-        self.write_prompt(user_prompt)
-        ai_answer = self.chat_answer()
-        print(ai_answer)
-        return user_prompt, ai_answer
-
-    def store_interaction_in_db(self, user_prompt, ai_answer):
-        user_embedding = self.embeddings_model.embed(user_prompt)
-        ai_answer_embedding = self.embeddings_model.embed(ai_answer)
-        self.interaction_id = self.interaction_id + 1
-        self.index.upsert(
-            [(self.interaction_id, user_embedding.tolist()), (self.interaction_id, ai_answer_embedding.tolist())]
+    def store_interaction_in_db(self, user_prompt, index, interaction):
+        user_embedding = self.embeddings_model.encode(user_prompt)
+        data_id = str(interaction) + '_' + datetime.now().strftime("%Y%m%d_%H%M%S")
+        index.upsert(
+            [(data_id, user_embedding.tolist(), {'original_question': user_prompt})]
         )
 
-    def learn_from_user_feedback(self, user_message, feedback):
-        sentiment = self.get_sentiment(feedback)
-        if sentiment > 0:
-            ai_answer = self.chat_answer()
-            self.store_interaction_in_db(user_message, ai_answer)
+    def prompt_for_retrieving_relevant_info(self, user_query):
 
-    def first_touch(self):
-        user_prompt = input("Talk to me!")
-        self.interaction_with_chat(user_prompt)
+        context = ''
+        query_vector = self.embeddings_model.encode(user_query)
+
+        # Preferences:
+        preferences_db = self.index_preferences.query(
+            vector=query_vector.tolist(),
+            top_k=10,
+            include_metadata=True
+        )
+
+        if not ('matches' not in preferences_db or len(preferences_db['matches']) == 0):
+            relevant_responses_preferences = [res['metadata']['original_question'] for res in preferences_db['matches']]
+            relevant_responses_preferences = list(set(relevant_responses_preferences))
+            relevant_responses_preferences = [i for i in relevant_responses_preferences if i != user_query]
+            context = context + ' ' + 'Preferências do usuário: ' + '\n'.join(relevant_responses_preferences) \
+                if relevant_responses_preferences else context
+
+        # Corrections:
+        corrections_db = self.index_corrections.query(
+            vector=query_vector.tolist(),
+            top_k=10,
+            include_metadata=True
+        )
+
+        if not ('matches' not in corrections_db or len(corrections_db['matches']) == 0):
+            relevant_responses_corrections = [res['metadata']['original_question'] for res in corrections_db['matches']]
+            relevant_responses_corrections = list(set(relevant_responses_corrections))
+            relevant_responses_corrections = [i for i in relevant_responses_corrections if i != user_query]
+            context = context + ' ' + 'Correções do usuário: ' + '\n'.join(relevant_responses_corrections) \
+                if relevant_responses_corrections else context
+
+        prompt = f"Baseado no seguinte contexto:\n{context}\n\nPergunta: {user_query}" if context != '' else user_query
+        return prompt
+
+    def informed_chat_answer(self, user_message):
+        prompt = self.prompt_for_retrieving_relevant_info(user_message)
+        ai_answer = self.chat.invoke(prompt)
+        return ai_answer.content
 
     def main(self):
-        self.first_touch()
         while True:
             user_prompt = input("Talk to me!")
-            self.learn_from_user_feedback(user_prompt)
+            self.interaction(user_prompt)
 
 
 a = ChatBot()
-a.write_prompt("Olá")
-a.chat_answer()
+a.main()
